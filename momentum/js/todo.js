1d96b98fd7b6982cf1e1455ed58e1958aea42fd7
Jaehee <brillar2225@gmail.com>
2022-05-07 02:52:30 +0900

    FIX: Change function to store in localStorage

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -3,38 +3,65 @@ const inputBox = document.querySelector('#input-box');
 const inputBtn = document.querySelector('#input-btn');
 const todoList = document.getElementById('todo-list');
 
-const todoItems = [];
+const TODOS_KEY = 'todoItems';
 
+let todoItems = [];
+
+// Save input value to localStorage
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todoItems));
+}
+
+// Delete lists
 function deleteTodo(event) {
   const item = this.parentElement;
+  todoItems = todoItems.filter((todoItem) => todoItem.id !== parseInt(item.id));
   item.remove();
+  saveTodos();
 }
 
+// Show lists
 function paintTodo(inputValue) {
-  if (inputValue.length > 0) {
-    const itemList = document.createElement('li');
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
+  const itemList = document.createElement('li');
+  itemList.id = inputValue.id;
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
 
-    const item = document.createElement('span');
-    item.innerHTML = inputValue;
+  const item = document.createElement('span');
+  item.innerHTML = inputValue.text;
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerHTML = '❎';
-    deleteBtn.addEventListener('click', deleteTodo);
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = '❎';
+  deleteBtn.addEventListener('click', deleteTodo);
 
-    itemList.appendChild(checkBox);
-    itemList.appendChild(item);
-    itemList.appendChild(deleteBtn);
-    todoList.appendChild(itemList);
-  }
+  itemList.appendChild(checkBox);
+  itemList.appendChild(item);
+  itemList.appendChild(deleteBtn);
+  todoList.appendChild(itemList);
 }
 
+// Get input value
 function getTodoInput(event) {
   event.preventDefault();
   const inputValue = inputBox.value;
   inputBox.value = '';
-  paintTodo(inputValue);
+  const todoItemObj = {
+    text: inputValue,
+    id: Date.now(),
+  };
+  todoItems.push(todoItemObj);
+  // Call paintTodo and saveTodo function
+  paintTodo(todoItemObj);
+  saveTodos();
 }
 
 todoInput.addEventListener('submit', getTodoInput);
+
+// Get key value from localStorage
+const savedTodos = localStorage.getItem(TODOS_KEY);
+// Parse key value stringified and Excute paintTodo function for each value
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todoItems = parsedTodos;
+  parsedTodos.forEach(paintTodo);
+}
